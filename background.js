48fb01c7d1ebe88a2001814632419c7bb03e5a8c
Jason B. Cox <contact@jasonbcox.com>
2014-01-29 08:46:59 +0000

    Fixed domain tracking checks to restrict the plugin to only operate on tracked domains

--- background.js
+++ background.js
@@ -4,8 +4,9 @@ function domainIsTracked( thisDomain, domains ) {
 		var url = thisDomain;
 		var check = domains[i];
 		if ( check.charAt( check.length - 1 ) == '*' ) {
-			check = check.substr( 0, check.length );
-			url = url.substr( 0, check.length );
+			var len = check.length - 1;
+			check = check.substr( 0, len );
+			url = url.substr( 0, len );
 		}
 		if ( url == check ) return true;
 	}
@@ -17,13 +18,15 @@ chrome.extension.onMessage.addListener( function ( request, sender, sendResponse
 		var domains = [];
 		var isTracked = false;
 		chrome.storage.local.get( 'domains', function ( items ) {
-			if ( items instanceof Array ) domains = items;
+			if ( items.domains instanceof Array ) domains = items.domains;
 			isTracked = domainIsTracked( request.domain, domains );
-			chrome.storage.local.get( 'words', function ( words ) {
-				chrome.tabs.getSelected( null, function ( tab ) {
-					chrome.tabs.sendMessage( tab.id, { action: 'activate', data: words } );
+			if ( isTracked == true ) {
+				chrome.storage.local.get( 'words', function ( words ) {
+					chrome.tabs.getSelected( null, function ( tab ) {
+						chrome.tabs.sendMessage( tab.id, { action: 'activate', data: words } );
+					} );
 				} );
-			} );
+			}
 		} );
 		sendResponse( {} );
 	} else {
